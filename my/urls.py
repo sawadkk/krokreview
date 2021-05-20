bb644523197c53d88aff5e9bc9ad3cf3ab27883e
sawad_kk <ssawad09@gmail.com>
2021-05-20 10:40:41 +0530

    make

--- my/urls.py
+++ my/urls.py
@@ -1,21 +1,55 @@
-"""website URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+try:
+    from django.conf.urls import url
+except ImportError:
+    from django.urls import re_path as url
+from quiz.views import QuizListView, CategoriesListView, \
+    ViewQuizListByCategory, QuizUserProgressView, QuizMarkingList, \
+    QuizMarkingDetail, QuizDetailView, QuizTake
+
+
+
+
+
 urlpatterns = [
-path('', include('hello.urls')),
-path('admin/', admin.site.urls),
-]
+    
+    path('admin/', admin.site.urls),
+    path('hello', include('hello.urls')),
+    path('account',include('account.urls')),
+    
+    url(r'^$',
+        view=QuizListView.as_view(),
+        name='quiz_index'),
+
+    url(r'^category/$',
+        view=CategoriesListView.as_view(),
+        name='quiz_category_list_all'),
+
+    url(r'^category/(?P<category_name>[\w|\W-]+)/$',
+        view=ViewQuizListByCategory.as_view(),
+        name='quiz_category_list_matching'),
+
+    url(r'^progress/$',
+        view=QuizUserProgressView.as_view(),
+        name='quiz_progress'),
+
+    url(r'^marking/$',
+        view=QuizMarkingList.as_view(),
+        name='quiz_marking'),
+
+    url(r'^marking/(?P<pk>[\d.]+)/$',
+        view=QuizMarkingDetail.as_view(),
+        name='quiz_marking_detail'),
+
+    #  passes variable 'quiz_name' to quiz_take view
+    url(r'^(?P<slug>[\w-]+)/$',
+        view=QuizDetailView.as_view(),
+        name='quiz_start_page'),
+
+    url(r'^(?P<quiz_name>[\w-]+)/take/$',
+        view=QuizTake.as_view(),
+        name='quiz_question'),
+]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
